3e6b5051dac4299aa07d8d05e2a3f401565187ed
Titus von der Malsburg <malsburg@posteo.de>
2025-03-11 10:47:18 +0100

    Added larger versions of bloom and xglm

--- llm_generate.py
+++ llm_generate.py
@@ -38,8 +38,13 @@ import torch.nn.functional as F
 
 models = {
   "gpt2":       ("openai-community/gpt2", GPT2LMHeadModel),
+  "gpt2-large": ("openai-community/gpt2-large", GPT2LMHeadModel),
   "bloom-560m": ("bigscience/bloom-560m", BloomForCausalLM),
-  "xglm-564M":  ("facebook/xglm-564M", XGLMForCausalLM)
+  "bloom-1b7":  ("bigscience/bloom-1b7", BloomForCausalLM),
+  "bloom-3b":   ("bigscience/bloom-3b", BloomForCausalLM),
+  "xglm-564M":  ("facebook/xglm-564M", XGLMForCausalLM),
+  "xglm-1.7B":  ("facebook/xglm-1.7B", XGLMForCausalLM),
+  "xglm-2.9B":  ("facebook/xglm-2.9B", XGLMForCausalLM)
 }
 model, model_class = models[args.model]
 
