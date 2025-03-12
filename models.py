9202b03e03c9315fa5f1c0da3606cdbbc991534a
Titus von der Malsburg <malsburg@posteo.de>
2025-03-12 10:25:39 +0100

    Added german-gpt2-larger

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 
 models = {
-  "gpt2":        ("openai-community/gpt2", "GPT2LMHeadModel"),
-  "gpt2-large":  ("openai-community/gpt2-large", "GPT2LMHeadModel"),
-  "german-gpt2": ("dbmdz/german-gpt2", "AutoModelForCausalLM"),
-  "bloom-560m":  ("bigscience/bloom-560m", "BloomForCausalLM"),
-  "bloom-1b7":   ("bigscience/bloom-1b7", "BloomForCausalLM"),
-  "bloom-3b":    ("bigscience/bloom-3b", "BloomForCausalLM"),
-  "xglm-564M":   ("facebook/xglm-564M", "XGLMForCausalLM"),
-  "xglm-1.7B":   ("facebook/xglm-1.7B", "XGLMForCausalLM"),
-  "xglm-2.9B":   ("facebook/xglm-2.9B", "XGLMForCausalLM")
+  "gpt2":               ("openai-community/gpt2",        "GPT2LMHeadModel"),
+  "gpt2-large":         ("openai-community/gpt2-large",  "GPT2LMHeadModel"),
+  "bloom-560m":         ("bigscience/bloom-560m",        "BloomForCausalLM"),
+  "bloom-1b7":          ("bigscience/bloom-1b7",         "BloomForCausalLM"),
+  "bloom-3b":           ("bigscience/bloom-3b",          "BloomForCausalLM"),
+  "xglm-564M":          ("facebook/xglm-564M",           "XGLMForCausalLM"),
+  "xglm-1.7B":          ("facebook/xglm-1.7B",           "XGLMForCausalLM"),
+  "xglm-2.9B":          ("facebook/xglm-2.9B",           "XGLMForCausalLM"),
+  "german-gpt2":        ("dbmdz/german-gpt2",            "AutoModelForCausalLM"),
+  "german-gpt2-larger": ("stefan-it/german-gpt2-larger", "AutoModelForCausalLM"),
 }
